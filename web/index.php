f6dfd49b18717b334ca939314c12628dddbeb089
iproman <itechman@yandex.ru>
2019-06-15 15:21:57 +0500

    Base config of index.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,7 @@
-Our CRM
+<?php
+// Включаем сам фреймворк Yii
+require(__DIR__ . './../vendor/yiisoft/yii2/Yii.php');
+// Получаем конфигурацию
+$config = require(__DIR__ . '/../config/web.php');
+// Создаем и немедленно выполняем приложение
+(new \yii\web\Application($config))->run();
