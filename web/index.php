690998be0e157d66d3cc8fd87c571640197128b5
iproman <itechman@yandex.ru>
2019-06-16 07:51:22 +0500

    Add constant for debug mode.

--- web/index.php
+++ web/index.php
@@ -1,7 +1,13 @@
 <?php
+
+//ini_set('display_errors', true);
+define('YII_DEBUG', true);
+
 // Включаем сам фреймворк Yii
 require(__DIR__ . './../vendor/yiisoft/yii2/Yii.php');
+
 // Получаем конфигурацию
 $config = require(__DIR__ . '/../config/web.php');
+
 // Создаем и немедленно выполняем приложение
 (new \yii\web\Application($config))->run();
