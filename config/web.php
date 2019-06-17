44c69947670fa4fcf7f0c00207bb7b11a3521130
iproman <itechman@yandex.ru>
2019-06-17 22:32:08 +0500

    Add db config for web config.

--- config/web.php
+++ config/web.php
@@ -3,6 +3,7 @@ return [
     'id' => 'crmapp',
     'basePath' => realpath(__DIR__ . '/../'),
     'components' => [
+        'db' => require(__DIR__ . '/db.php'),
         'request' => [
             'cookieValidationKey' => 'cookie secret key',
         ]
