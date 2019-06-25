522c1ae7e9a7c1c458e92f8923dbd1d725e77724
iproman <itechman@yandex.ru>
2019-06-25 10:48:17 +0500

    Change number in query page, not passed test.

--- views/customers/query.php
+++ views/customers/query.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 
 echo Html::beginForm(['/customers'], 'get');
-echo Html::label('Phone number to search: ', 'phone_number');
-echo Html::textInput('PhoneRecord[number]');
+echo Html::label('Phone number to search: ', 'number');
+echo Html::textInput('number');
 echo Html::submitButton('Search');
 echo Html::endForm();
