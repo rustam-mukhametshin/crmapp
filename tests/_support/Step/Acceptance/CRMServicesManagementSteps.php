9b744c3c074f71112c09f72f9c4ec9be1a8898ae
iproman <itechman@yandex.ru>
2019-07-01 07:33:16 +0500

    Add imageService method for tests.

--- tests/_support/Step/Acceptance/CRMServicesManagementSteps.php
+++ tests/_support/Step/Acceptance/CRMServicesManagementSteps.php
@@ -4,5 +4,13 @@ namespace Step\Acceptance;
 class CRMServicesManagementSteps extends CRMGuestSteps
 {
 
+    function imagineService()
+    {
+        $faker = \Faker\Factory::create();
+        return [
+            'ServiceRecord[name]' => $faker->sentence($words = 3),
+            'ServiceRecord[hourly_rate]' => $faker->randomNumber($digits = 2),
+        ];
+    }
 
 }
