bbb325f1b3559bd9245003eefe59d15cd2b70cb9
iproman <itechman@yandex.ru>
2019-07-05 10:38:26 +0500

    Add actionDocs to siteController.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,8 +6,19 @@ use yii\web\Controller;
 
 class SiteController extends Controller
 {
+    /**
+     * @return string
+     */
     public function actionIndex()
     {
         return 'Our CRM';
+    }
+
+    /**
+     * @return string
+     */
+    public function actionDocs()
+    {
+        return $this->render('docindex.md');
     }
 }
