6debac076e795e278279fb34381bb678274e4851
iproman <itechman@yandex.ru>
2019-07-13 00:27:45 +0500

    Add UserRecord model.

---
+++ models/user/UserRecord.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models\user;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ */
+class UserRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+        ];
+    }
+}
