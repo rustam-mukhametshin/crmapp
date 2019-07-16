3d8edbfca0b9f97bd80d82f16dc089f1f34b4efc
iproman <itechman@yandex.ru>
2019-07-16 05:34:44 +0500

    Generated PasswordHashingTest.

---
+++ tests/functional/PasswordHashingTest.php
@@ -0,0 +1,22 @@
+<?php 
+class PasswordHashingTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \FunctionalTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
