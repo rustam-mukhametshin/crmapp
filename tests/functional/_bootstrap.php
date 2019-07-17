7cbf2585561c2088d6278dc4c644db6a0871518e
iproman <itechman@yandex.ru>
2019-07-17 11:08:03 +0500

    Add bootstrap for functional.

---
+++ tests/functional/_bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+
+new \yii\web\Application(
+    require(__DIR__ . '/../../config/web.php')
+);
