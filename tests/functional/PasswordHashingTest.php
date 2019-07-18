57c41c283865e75725c41f889ab61b0ef4fdf52d
iproman <itechman@yandex.ru>
2019-07-18 07:42:20 +0500

    Add test method of checking pass hashed.

--- tests/functional/PasswordHashingTest.php
+++ tests/functional/PasswordHashingTest.php
@@ -1,11 +1,17 @@
-<?php 
-class PasswordHashingTest extends \Codeception\Test\Unit
+<?php
+
+use app\models\user\UserRecord;
+use Faker\Factory;
+use yii\base\Security;
+use Codeception\Test\Unit;
+
+class PasswordHashingTest extends Unit
 {
     /**
      * @var \FunctionalTester
      */
     protected $tester;
-    
+
     protected function _before()
     {
     }
@@ -14,9 +20,32 @@ class PasswordHashingTest extends \Codeception\Test\Unit
     {
     }
 
-    // tests
-    public function testSomeFeature()
+    public function PasswordIsHashedWhenSavingUser()
     {
+        $user = $this->imagineUserRecord();
+        $plaintext_passowrd = $user->password;
+        $user->save();
 
+        $saved_user = UserRecord::findOne($user->id);
+
+        $security = new Security();
+        $this->assertInstanceOf(get_class($user), $saved_user);
+        $this->assertTrue(
+            $security->validatePassword(
+                $plaintext_passowrd,
+                $saved_user->password
+            )
+        );
     }
+
+    public function imagineUserRecord()
+    {
+        $faker = Factory::create();
+
+        $user = new UserRecord();
+        $user->username = $faker->word;
+        $user->password = $faker->md5(time());
+        return $user;
+    }
+
 }
