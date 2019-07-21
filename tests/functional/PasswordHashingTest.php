221fc8cc7fa3352980f013d6af8c836f30eb07f9
iproman <itechman@yandex.ru>
2019-07-21 12:34:29 +0500

    Add test checking of rehashed pass after update.

--- tests/functional/PasswordHashingTest.php
+++ tests/functional/PasswordHashingTest.php
@@ -48,4 +48,23 @@ class PasswordHashingTest extends Unit
         return $user;
     }
 
+    public function PasswordIsNotRehashedAfterUpdatingWithoutChangingPassword()
+    {
+        $user = $this->imagineUserRecord();
+        $user->save();
+
+        /** @var UserRecord $saved_user */
+        $saved_user = UserRecord::findOne($user->id);
+        $expected_hash = $saved_user->password;
+
+        $saved_user->username = md5(time());
+        $saved_user->save();
+
+        /** @var UserRecord $updated_user */
+        $updated_user = UserRecord::findOne($saved_user->id);
+
+        $this->assertEquals($expected_hash, $saved_user->password);
+        $this->assertEquals($expected_hash, $updated_user->password);
+    }
+
 }
