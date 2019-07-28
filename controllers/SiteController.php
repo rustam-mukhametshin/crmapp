744b82950f93cf4ca5f98ecbb88ca117c41e610a
iproman <itechman@yandex.ru>
2019-07-28 22:49:04 +0500

    Add actionLogin in SiteController.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\user\LoginForm;
 
 class SiteController extends Controller
 {
@@ -20,5 +21,21 @@ class SiteController extends Controller
     public function actionDocs()
     {
         return $this->render('docindex.md');
+    }
+
+    /**
+     * @return string|\yii\web\Response
+     */
+    public function actionLogin()
+    {
+        if (!\Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+        $model = new LoginForm();
+        if ($model->load(\Yii::$app->request->post()) and $model->login()) {
+            return $this->goBack();
+        }
+
+        return $this->render('login', compact('model'));
     }
 }
