215a31030c1d5038de9e0f2bdd5288ddb01b5fcf
iproman <itechman@yandex.ru>
2019-08-05 08:09:02 +0500

    Addition test methods for LoginAndLogoutCept.

--- tests/_support/Step/Acceptance/CRMUsersManagementSteps.php
+++ tests/_support/Step/Acceptance/CRMUsersManagementSteps.php
@@ -58,4 +58,106 @@ class CRMUsersManagementSteps extends CRMGuestSteps
         //$I->wait(1);
     }
 
+    public function seeIAmInListUsersUi()
+    {
+        $I = $this;
+        $I->seeCurrentUrlMatches('/users/'); // this is regexp , not an URL
+        $I->seeElement(self::SERVICES_LIST_SELECTOR);
+    }
+
+    public function seeIamInViewUserUi()
+    {
+        $I = $this;
+        $I->seeCurrentUrlMatches('~users/view~'); // this is regexp, not an URL
+        $I->see('Update');
+        $I->see('Delete');
+    }
+
+    public function seeEditUserUi()
+    {
+        $I = $this;
+        $I->seeCurrentUrlMatches('~users/update~'); // this is regexp, not an URL
+        $I->see('Update');
+    }
+
+    public function dontSeeUserInList($user_data)
+    {
+        $I = $this;
+        $I->dontSee($user_data['UserRecord[username]'],
+            self::SERVICES_LIST_SELECTOR
+        );
+    }
+
+    public function seeEditButtonBesideUser($user_data)
+    {
+        $I = $this;
+        $xpath = $this->makeXpathForButtonNearUserName(
+            $user_data['UserRecord[username]'],
+            'Update'
+        );
+        $I->seeElement($xpath);
+    }
+
+    private function makeXpathForButtonNearUserName(
+        $user_name,
+        $button_title
+    )
+    {
+        $xpath = sprintf(
+            '//td[text()="%s"]/following-sibling::td/a[@title="%s"]',
+            $user_name,
+            $button_title
+        );
+        return $xpath;
+    }
+
+    public function clickEditButtonBesideUser($user_data)
+    {
+        $I = $this;
+        $xpath = $this->makeXpathForButtonNearUserName(
+            $user_data['UserRecord[username]'],
+            'Update'
+        );
+        $I->click($xpath);
+    }
+
+    public function seeDeletionConfirmation()
+    {
+        $I = $this;
+        $I->seeInPopup('delete');
+    }
+
+    public function cancelDeletion()
+    {
+        $I = $this;
+        //$I->cancelPopup();
+    }
+
+    public function confirmDeletion()
+    {
+        $I = $this;
+        //$I->acceptPopup();
+    }
+
+    public function seeDeleteButtonBesideUser($user_data)
+    {
+        $I = $this;
+        $xpath = $this->makeXpathForButtonNearUserName(
+            $user_data['UserRecord[username]'],
+            'Delete'
+        );
+        $I->seeElement($xpath);
+    }
+
+    public function clickDeleteButtonBesideUser($user_data)
+    {
+        $I = $this;
+        $xpath = $this->makeXpathForButtonNearUserName(
+            $user_data['UserRecord[username]'],
+            'Delete'
+        );
+        $I->click($xpath);
+        //$I->wait(1);
+    }
+
 }
