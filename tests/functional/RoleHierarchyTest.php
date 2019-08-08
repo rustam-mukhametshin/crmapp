25b59a4c8872585b5d5c38140d9f8ca24a64d018
iproman <itechman@yandex.ru>
2019-08-09 03:18:27 +0500

    Get user for test from UserRecord.

--- tests/functional/RoleHierarchyTest.php
+++ tests/functional/RoleHierarchyTest.php
@@ -1,14 +1,19 @@
 <?php
 
+use yii\web\User;
+
 class RoleHierarchyTest extends \Codeception\Test\Unit
 {
     /**
      * @var \FunctionalTester
      */
     protected $tester;
+    /** @var User */
+    private $user;
 
     protected function _before()
     {
+        $this->user = Yii::$app->user;
     }
 
     protected function _after()
