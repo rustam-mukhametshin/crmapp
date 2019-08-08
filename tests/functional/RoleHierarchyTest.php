6b9fedb64f01dd8e9e8f97d79f6f917911222e40
iproman <itechman@yandex.ru>
2019-08-08 14:04:01 +0500

    Generate functional RoleHierarchyTest.

---
+++ tests/functional/RoleHierarchyTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class RoleHierarchyTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \FunctionalTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
