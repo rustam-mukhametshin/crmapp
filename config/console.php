0f6a4736a1b546e50f3da8a7f51c8c36bb7f5c8c
iproman <itechman@yandex.ru>
2019-08-11 16:23:56 +0500

    Attach rbac base on db to web/console.

--- config/console.php
+++ config/console.php
@@ -4,5 +4,9 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'db' => require(__DIR__ . '/db.php'),
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest'],
+        ],
     ],
 ];
--- config/web.php
+++ config/web.php
@@ -36,6 +36,10 @@ return [
         'user' => [
             'identityClass' => 'app\models\user\UserRecord',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest'],
+        ],
     ],
     'modules' => [
         'gii' => [
