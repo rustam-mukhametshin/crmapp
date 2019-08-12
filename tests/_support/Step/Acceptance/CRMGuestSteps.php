5940c09e282428e35c5d77baa374691017de5815
iproman <itechman@yandex.ru>
2019-08-12 08:48:46 +0500

    Add test login logout methods.

--- tests/_support/Step/Acceptance/CRMGuestSteps.php
+++ tests/_support/Step/Acceptance/CRMGuestSteps.php
@@ -4,4 +4,22 @@ namespace Step\Acceptance;
 class CRMGuestSteps extends \AcceptanceTester
 {
 
+    function login($username, $password)
+    {
+        $I = $this;
+        $I->amOnPage('/site/login');
+        $I->fillField('LoginForm[username]', $username);
+        $I->fillField('LoginForm[password]', $password);
+        $I->click('Login');
+        //$I->wait(1);
+        $I->seeCurrentUrlEquals('/');
+    }
+
+    function logout()
+    {
+        $I = $this;
+        $I->amOnPage('/');
+        // Expecting that this button is presented on the homepage.
+        $I->click('logout');
+    }
 }
