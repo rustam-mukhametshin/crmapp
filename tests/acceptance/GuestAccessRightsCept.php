c4ae0783c509cf372c97c3f8f34745b7190a0c61
iproman <itechman@yandex.ru>
2019-08-14 11:14:48 +0500

    Add base test steps for GuestLevelAccess.

--- tests/acceptance/GuestAccessRightsCept.php
+++ tests/acceptance/GuestAccessRightsCept.php
@@ -1,3 +1,35 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+<?php
+
+use Step\Acceptance\CRMGuestSteps;
+
+$I = new CRMGuestSteps($scenario);
+$I->wantTo('check guest access rights');
+
+$I->amOnPage('/customers/query');
+$I->seeElement('#login-form');
+
+$I->amOnPage('/customers/index');
+$I->seeElement('#login-form');
+
+
+$I->amOnPage('/customers/add');
+$I->seeElement('#login-form');
+
+$I->amOnPage('/services/create');
+$I->seeElement('#login-form');
+
+$I->amOnPage('/services/index');
+$I->seeElement('#login-form');
+
+$I->amOnPage('/services/view');
+$I->seeElement('#login-form');
+
+
+$I->amOnPage('/users/create');
+$I->seeElement('#login-form');
+
+$I->amOnPage('/users/index');
+$I->seeElement('#login-form');
+
+$I->amOnPage('/users/view');
+$I->seeElement('#login-form');
