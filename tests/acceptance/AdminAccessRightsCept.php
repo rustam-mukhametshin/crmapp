9ea2f55e4cda8214d65960c36d0df4aef1bb11dc
iproman <itechman@yandex.ru>
2019-08-14 11:00:16 +0500

    Add base test steps for AdminLevelAccess.

--- tests/acceptance/AdminAccessRightsCept.php
+++ tests/acceptance/AdminAccessRightsCept.php
@@ -1,3 +1,18 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+<?php
+
+use Step\Acceptance\CRMUsersManagementSteps;
+
+$I = new CRMUsersManagementSteps($scenario);
+$I->wantTo('check Admin-level access rights');
+
+$I->amOnPage('/customers/query');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/users/create');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('users/index');
+$I->dontSee('Forbidden');
