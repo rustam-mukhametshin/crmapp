fdda03764835cefa5b37f8b636f14c88fb74df82
iproman <itechman@yandex.ru>
2019-08-14 11:10:19 +0500

    Add base test steps for ManagerLevelAccess.

--- tests/acceptance/ManagerAccessRightsCept.php
+++ tests/acceptance/ManagerAccessRightsCept.php
@@ -1,3 +1,73 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+<?php
+
+use Step\Acceptance\CRMOperatorSteps;
+use Step\Acceptance\CRMServicesManagementSteps;
+
+$I = new CRMOperatorSteps($scenario);
+$I->wantTo('check Manager-level access rights');
+
+$I->amOnPage('/customers/query');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/add');
+$I->dontSee('Forbidden');
+
+
+$I->amOnPage('/services/create');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/view');
+$I->dontSee('Forbidden');
+
+
+$I->amOnPage('/users/create');
+$I->see('Forbidden');
+
+$I->amOnPage('/users/index');
+$I->see('Forbidden');
+
+$I->amOnPage('/users/view');
+$I->see('Forbidden');
+
+$I->logout();
+
+
+$I = new CRMServicesManagementSteps($scenario);
+$I->wantTo('Check Manager-level access rights for Services management');
+
+$I->amOnPage('/customers/query');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/add');
+$I->dontSee('Forbidden');
+
+
+$I->amOnPage('/services/create');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/view');
+$I->dontSee('Forbidden');
+
+
+$I->amOnPage('/users/create');
+$I->see('Forbidden');
+
+$I->amOnPage('/users/index');
+$I->see('Forbidden');
+
+$I->amOnPage('/users/view');
+$I->see('Forbidden');
+
+$I->logout();
