f0e44390d507af2cd71d91a2d5a6a6a990676ed4
iproman <itechman@yandex.ru>
2019-08-14 11:13:16 +0500

    Add base test steps for UserLevelAccess.

--- tests/acceptance/UserAccessRightsCept.php
+++ tests/acceptance/UserAccessRightsCept.php
@@ -1,3 +1,35 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+<?php
+
+use Step\Acceptance\CRMUserSteps;
+
+$I = new CRMUserSteps($scenario);
+$I->wantTo('Check User-level access rights');
+
+$I->amOnPage('/customers/query');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/add');
+$I->see('Forbidden');
+
+
+$I->amOnPage('/services/create');
+$I->see('Forbidden');
+
+$I->amOnPage('/services/index');
+$I->see('Forbidden');
+
+$I->amOnPage('/services/view');
+$I->see('Forbidden');
+
+
+$I->amOnPage('/users/create');
+$I->see('Forbidden');
+
+$I->amOnPage('/users/index');
+$I->see('Forbidden');
+
+$I->amOnPage('/users/view');
+$I->see('Forbidden');
